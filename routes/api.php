ebde5cda3d721a9d2b89d6a93161f9226d428f05
Liam <anari@Air-van-Liam.home>
2025-03-22 21:48:31 +0100

    Add routes to api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ProjectController;
+use App\Http\Controllers\API\TaskController;
+use App\Http\Controllers\API\CommentController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Project routes
+    Route::apiResource('projects', ProjectController::class);
+    
+    // Task routes
+    Route::apiResource('tasks', TaskController::class);
+    Route::post('tasks/update-priorities', [TaskController::class, 'updatePriorities']);
+    
+    // Comment routes
+    Route::apiResource('comments', CommentController::class);
 });
