87f0f169b9b3e233628bbd222a899d7d98cba556
Liam <anari@Air-van-Liam.home>
2025-03-23 16:31:21 +0100

    modify TaskUpdated

--- app/Events/TaskUpdated.php
+++ app/Events/TaskUpdated.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\Task;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -10,16 +11,18 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class TaskUpdated
+class TaskUpdated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $task;
 
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(Task $task)
     {
-        //
+        $this->task = $task;
     }
 
     /**
@@ -30,7 +33,7 @@ class TaskUpdated
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('channel-name'),
+            new PrivateChannel('project.' . $this->task->project_id),
         ];
     }
 }
